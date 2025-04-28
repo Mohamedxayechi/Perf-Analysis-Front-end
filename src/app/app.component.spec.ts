065ffe0453a6ee620c496e5abbab4976ada8fe5c
Mohamedxayechi <perf-analysis.mohamed.ayechi@outlook.com>
2025-04-28 11:22:43 +0100

    corrected version and remove side server

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-test' title`, () => {
+  it(`should have the 'Perf-analysis-Front-End' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-test');
+    expect(app.title).toEqual('Perf-analysis-Front-End');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-test');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Perf-analysis-Front-End');
   });
 });
