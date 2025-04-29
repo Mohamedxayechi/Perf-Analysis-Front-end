338345b0802d0efff6f22b75df20ab825d34eba8
bengayed <perf-analysis-mouhamed.bengayed86@outlook.com>
2025-04-29 16:25:39 +0100

    change in files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Feature/graphical_layout/components/home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
+
+import { FormProjectComponent } from './Feature/createProject/components/form-project/form-project.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ListProjectComponent } from './Feature/createProject/components/list-project/list-project.component';
+import { UbloadFileComponent } from './Feature/uploadFile/ubload-file/ubload-file.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent, pathMatch: 'full' }, // Use HomeComponent for root
+    { path: '', component: HomeComponent, pathMatch: 'full' }, 
+    { path: 'create/project', component: FormProjectComponent }, 
+    { path: 'list/project', component: ListProjectComponent }, 
+    { path: 'ubload/vd', component: UbloadFileComponent }, 
+
+
 
 ];
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes),HttpClientModule],
     exports: [RouterModule],
   })
   export class AppRoutingModule {}
