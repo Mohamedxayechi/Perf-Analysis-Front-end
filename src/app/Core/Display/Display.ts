29b308a855d4e2d1788c6134e1b61d3285770e48
Amine ouhichi <perf-analysis.amine.ouhichi@outlook.com>
2025-05-02 16:27:01 +0100

    Update Display

--- src/app/Core/Display/Display.ts
+++ src/app/Core/Display/Display.ts
@@ -107,6 +107,7 @@ export class Display {
         } catch (error) {
           const message = error instanceof Error ? error.message : String(error);
           console.error(`Error updating shapes: ${message}`);
+          console.log("")
         }
         break;
       }
